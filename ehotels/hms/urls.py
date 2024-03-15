781a77859dbd63feca111bdfa77ed59566be2cd3
malsa080 <malsa080@uottawa.ca>
2024-03-15 16:31:43 -0400

    clean up

--- ehotels/hms/urls.py
+++ ehotels/hms/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        # path("", views.index, name="index"),
         path('register', views.register_client),
         path('login', views.login_client),
         path('search_rooms', views.search_rooms)

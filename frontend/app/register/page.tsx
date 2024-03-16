60e9cfb6ff3f96f57b058850a02454263f920171
ghaitben <ghaitben2017@gmail.com>
2024-03-16 11:02:16 -0400

    [Minor] Typo

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -347,8 +347,8 @@ export default function RegistrationForm() {
                           </select>
                         </div>
                         <div>
-                            <label htmlFor="postalcode" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postalcode</label>
-                            <input type="text" name="postalcode" id="postalcode" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="required" />
+                            <label htmlFor="postal_code" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postalcode</label>
+                            <input type="text" name="postal_code" id="postal_code" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required="required" />
                         </div>
 
                         <div className="flex items-start">
